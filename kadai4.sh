d61b2b7429d9c98966b900a0153e664ec307a88b
hideharuouchi <a1705ho@aiit.ac.jp>
2018-06-12 01:30:49 +0900

    Update kadai4.sh

--- kadai4.sh
+++ kadai4.sh
@@ -1,25 +1,32 @@
 #!/bin/sh
 
+tmp=/tmp/$$
+
+ERROR_EXIT (){
+  echo "$1" >&2
+  rm -f $tmp-*
+  exit 1
+}
 
 #TEST1-1:
 echo "5 is natural number" > /tmp/$$-ans
-./kadai3.sh 5 > /tmp/$$-out || ERROR_EXIT "exe chigau"
-diff /tmp/$$-ans /tmp/$$-out || ERROR_EXIT "diff chigau"
+./kadai3.sh 5 > $tmp-out || ERROR_EXIT "exe chigau"
+diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
 
 #TEST1-2:
-echo "10 is not natural number" > /tmp/$$-ans
-./kadai3.sh 10 > /tmp/$$-out || ERROR_EXIT "exe chigau"
-diff /tmp/$$-ans /tmp/$$-out || ERROR_EXIT "diff chigau"
+echo "10 is not natural number" > $tmp-ans
+./kadai3.sh 10 > $tmp-out || ERROR_EXIT "exe chigau"
+diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
 
 #TEST1-3:
-echo "-2 is not natural number" > /tmp/$$-ans
-./kadai3.sh -2 > /tmp/$$-out || ERROR_EXIT "exe chigau"
-diff /tmp/$$-ans /tmp/$$-out || ERROR_EXIT "diff chigau"
+echo "-2 is not natural number" > $tmp-ans
+./kadai3.sh -2 > $tmp-out || ERROR_EXIT "exe chigau"
+diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
 
 #TEST1-4:
-echo "13 is natural number" > /tmp/$$-ans
-./kadai3.sh 13 > /tmp/$$-out || ERROR_EXIT "exe chigau"
-diff /tmp/$$-ans /tmp/$$-out || ERROR_EXIT "diff chigau"
+echo "13 is natural number" > $tmp-ans
+./kadai3.sh 13 > $tmp-out || ERROR_EXIT "exe chigau"
+diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
 
 
 echo OK
