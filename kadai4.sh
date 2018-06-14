8a88bee0772a51f698e22c83144d23b02eeb1553
hideharuouchi <a1705ho@aiit.ac.jp>
2018-06-14 20:32:58 +0900

    Update kadai4.sh

--- kadai4.sh
+++ kadai4.sh
@@ -10,7 +10,7 @@ ERROR_EXIT (){
 
 #TEST1   #1以下の数字についてチェック
 
-for ((i=-5; i<2; i++)); do
+for ((i=-5; i<2; i++)) do
   echo "$i is not natural number" > $tmp-ans
   ./kadai3.sh $i > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
@@ -21,7 +21,7 @@ TEST2  #素数についてチェック
 declare -a array = () 
 declare -a array = ("2" "3" "5" "7" "11" "13" "17" "19")  
 
-for ((i=0; i<8; i++)); do
+for ((i=0; i<8; i++)) do
   echo "$array[i] is natural number" > $tmp-ans
   ./kadai3.sh $array[i] > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
@@ -32,7 +32,7 @@ TEST3  #2以上の素数以外の数についてチェック
 declare -a array = () 
 declare -a array = ("4" "6" "8" "9" "10" "12" "14")
 
-for ((i=0; i<7; i++)); do
+for ((i=0; i<7; i++)) do
   echo "$array[i] not natural number" > $tmp-ans
   ./kadai3.sh $array[i] > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
