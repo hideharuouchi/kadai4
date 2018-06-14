6098b01c5b766d06a5380cbbb96e0f9930344acf
hideharuouchi <a1705ho@aiit.ac.jp>
2018-06-14 18:42:06 +0900

    Update kadai4.sh

--- kadai4.sh
+++ kadai4.sh
@@ -10,9 +10,9 @@ ERROR_EXIT (){
 
 #TEST1   #1以下の数字についてチェック
 
-ARRAY=( -5 -4 -3 -2 -1 0 1 )
+array=("-5" "-4" "-3" "-2" "-1" "0" "1")
 
-for item in ${ARRAY[@]}; do
+for item in ${array[@]}; do
   echo "$item is not natural number" > $tmp-ans
   ./kadai3.sh $b > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
@@ -20,9 +20,9 @@ done
 
 TEST2  #素数についてチェック
 
-ARRAY=( 2 3 5 7 11 13 17 19 )  
+array=("2" "3" "5" "7" "11" "13" "17" "19")  
 
-for item in ${ARRAY[@]}; do
+for item in ${array[@]}; do
   echo "$item is natural number" > $tmp-ans
   ./kadai3.sh $b > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
@@ -30,9 +30,9 @@ done
 
 TEST3  #2以上の素数以外の数についてチェック
 
-ARRAY=( 4 6 8 9 10 12 14 )  
+array=("4" "6" "8" "9" "10" "12" "14")  
 
-for item in ${ARRAY[@]}; do
+for item in ${array[@]}; do
   echo "$item is not natural number" > $tmp-ans
   ./kadai3.sh $b > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
