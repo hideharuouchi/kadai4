d565e6ce52533ffacbd6cf67f91fa8228f60b76e
hideharuouchi <a1705ho@aiit.ac.jp>
2018-06-14 18:31:16 +0900

    Update kadai4.sh

--- kadai4.sh
+++ kadai4.sh
@@ -10,33 +10,30 @@ ERROR_EXIT (){
 
 #TEST1   #1以下の数字についてチェック
 
-array=("-5" "-4" "-3" "-2" "-1" "0" "1")
+ARRAY=(-5 -4 -3 -2 -1 0 1)
 
-for ((i=0; i<7; i++)) do
-  b=${array[i]}
-  echo "$b is not natural number" > $tmp-ans
+for item in ${ARRAY[@]}; do
+  echo "$item is not natural number" > $tmp-ans
   ./kadai3.sh $b > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
 done
 
 TEST2  #素数についてチェック
 
-array=("2" "3" "5" "7" "11" "13" "17" "19")  
+ARRAY=(2 3 5 7 11 13 17 19)  
 
-for ((i=0; i<8; i++)) do
-  b=${array[i]}
-  echo "$b is natural number" > $tmp-ans
+for item in ${ARRAY[@]}; do
+  echo "$item is natural number" > $tmp-ans
   ./kadai3.sh $b > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
 done
 
 TEST3  #2以上の素数以外の数についてチェック
 
-array=("4" "6" "8" "9" "10" "12" "14")  
+ARRAY=(4 6 8 9 10 12 14)  
 
-for ((i=0; i<7; i++)) do
-  b=${array[i]}
-  echo "$b is not natural number" > $tmp-ans
+for item in ${ARRAY[@]}; do
+  echo "$item is not natural number" > $tmp-ans
   ./kadai3.sh $b > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
 done
