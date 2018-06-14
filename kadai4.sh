7aea831fb2d50457b77ba017e4ef5a6851a3979c
hideharuouchi <a1705ho@aiit.ac.jp>
2018-06-14 18:22:12 +0900

    Update kadai4.sh

--- kadai4.sh
+++ kadai4.sh
@@ -8,80 +8,38 @@ ERROR_EXIT (){
   exit 1
 }
 
-#TEST1-1:
-echo "-1 is not natural number" > $tmp-ans
-./kadai3.sh -1 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+#TEST1   #1以下の数字についてチェック
 
-#TEST1-2:
-echo "0 is not natural number" > $tmp-ans
-./kadai3.sh 0 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+array=("-5" "-4" "-3" "-2" "-1" "0" "1")
 
-#TEST1-3:
-echo "1 is not natural number" > $tmp-ans
-./kadai3.sh 1 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+for ((i=0; i<7; i++)) do
+  b=${array[i]}
+  echo "$b is not natural number" > $tmp-ans
+  ./kadai3.sh $b > $tmp-out || ERROR_EXIT "exe chigau"
+  diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+done
 
-#TEST1-4:
-echo "2 is natural number" > $tmp-ans
-./kadai3.sh 2 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+TEST2  #素数についてチェック
 
-#TEST1-5:
-echo "3 is natural number" > $tmp-ans
-./kadai3.sh 3 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+array=("2" "3" "5" "7" "11" "13" "17" "19")  
 
-#TEST1-6:
-echo "4 is not natural number" > $tmp-ans
-./kadai3.sh 4 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+for ((i=0; i<8; i++)) do
+  b=${array[i]}
+  echo "$b is natural number" > $tmp-ans
+  ./kadai3.sh $b > $tmp-out || ERROR_EXIT "exe chigau"
+  diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+done
 
-#TEST1-7:
-echo "5 is natural number" > $tmp-ans
-./kadai3.sh 5 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+TEST3  #2以上の素数以外の数についてチェック
 
-#TEST1-8:
-echo "6 is not natural number" > $tmp-ans
-./kadai3.sh 6 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+array=("4" "6" "8" "9" "10" "12" "14")  
 
-#TEST1-9:
-echo "7 is natural number" > $tmp-ans
-./kadai3.sh 7 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
-
-#TEST1-10:
-echo "8 is not natural number" > $tmp-ans
-./kadai3.sh 8 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
-
-#TEST1-11:
-echo "9 is not natural number" > $tmp-ans
-./kadai3.sh 9 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
-
-#TEST1-12:
-echo "10 is not natural number" > $tmp-ans
-./kadai3.sh 10 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
-
-#TEST1-13:
-echo "11 is natural number" > $tmp-ans
-./kadai3.sh 11 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
-
-#TEST1-14:
-echo "12 is not natural number" > $tmp-ans
-./kadai3.sh 12 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
-
-#TEST1-15:
-echo "13 is natural number" > $tmp-ans
-./kadai3.sh 13 > $tmp-out || ERROR_EXIT "exe chigau"
-diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+for ((i=0; i<7; i++)) do
+  b=${array[i]}
+  echo "$b is not natural number" > $tmp-ans
+  ./kadai3.sh $b > $tmp-out || ERROR_EXIT "exe chigau"
+  diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
+done
 
 
 echo OK
