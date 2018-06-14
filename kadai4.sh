386a10374daa41037b60ed2cc7e10599d568f057
hideharuouchi <a1705ho@aiit.ac.jp>
2018-06-14 19:17:51 +0900

    Update kadai4.sh

--- kadai4.sh
+++ kadai4.sh
@@ -10,7 +10,7 @@ ERROR_EXIT (){
 
 #TEST1   #1以下の数字についてチェック
 
-array=("-5" "-4" "-3" "-2" "-1" "0" "1")
+array=$("-5" "-4" "-3" "-2" "-1" "0" "1")
 
 for ((i=0; i<${#array[@]}; i++)); do
   echo "$array[i] is not natural number" > $tmp-ans
@@ -20,7 +20,7 @@ done
 
 TEST2  #素数についてチェック
 
-array=("2" "3" "5" "7" "11" "13" "17" "19")  
+array=$("2" "3" "5" "7" "11" "13" "17" "19")  
 
 for ((i=0; i<${#array[@]}; i++)); do
   echo "$array[i] is natural number" > $tmp-ans
@@ -30,7 +30,7 @@ done
 
 TEST3  #2以上の素数以外の数についてチェック
 
-array=("4" "6" "8" "9" "10" "12" "14")
+array=$("4" "6" "8" "9" "10" "12" "14")
 
 for ((i=0; i<${#array[@]}; i++)); do
   echo "$array[i] not natural number" > $tmp-ans
