86b81d6236ace48d36ba4ab71ab054074cc437dc
hideharuouchi <a1705ho@aiit.ac.jp>
2018-06-16 14:38:58 +0900

    Update kadai4.sh

--- kadai4.sh
+++ kadai4.sh
@@ -14,7 +14,7 @@ ERROR_EXIT (){
 declare -a array = '([0]="-4" [1]="-3" [2]="-2" [3]="-1" [4]="0" [5]="1")'  
 
 for ((i=0; i<6; i++)) do
-  echo "$array[i] is not prime number" > $tmp-ans
+  echo "${array[i]} is not prime number" > $tmp-ans
   ./kadai3.sh $array[i] > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
 done
@@ -25,7 +25,7 @@ TEST2  #素数についてチェック
 declare -a array = '([0]="2" [1]="3" [2]="5" [3]="7" [4]="11" [5]="13" [6]="17" [7]="19")'  
 
 for ((i=0; i<8; i++)) do
-  echo "$array[i] is prime number" > $tmp-ans
+  echo "${array[i]} is prime number" > $tmp-ans
   ./kadai3.sh $array[i] > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
 done
@@ -36,7 +36,7 @@ TEST3  #2以上の素数以外の数についてチェック
 declare -a array = '([0]="4" [1]="6" [2]="8" [3]="9" [4]="10" [5]="12" [6]="14")'
 
 for ((i=0; i<7; i++)) do
-  echo "$array[i] not prime number" > $tmp-ans
+  echo "${array[i]} not prime number" > $tmp-ans
   ./kadai3.sh $array[i] > $tmp-out || ERROR_EXIT "exe chigau"
   diff $tmp-ans $tmp-out || ERROR_EXIT "diff chigau"
 done
